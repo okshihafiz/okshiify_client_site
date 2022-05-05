4ec48efd2078bb74edd4f6a47e4719bfceb4611c
shuvo794 <shuvohossain794@gmail.com>
2022-05-05 20:54:43 +0600

    firebase

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,10 +1,16 @@
 const firebaseConfig = {
-     apiKey: process.env.REACT_APP_API_KEY,
-     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-     projectId: process.env.REACT_APP_PROJECT_ID,
-     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-     appId: process.env.REACT_APP_APP_ID,
+    //  apiKey: process.env.REACT_APP_API_KEY,
+    //  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    //  projectId: process.env.REACT_APP_PROJECT_ID,
+    //  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    //  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    //  appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyD9PiWhRcBcNyKzTGAfqXhRfoDiqzBn2D8",
+    authDomain: "mega-shop-c110e.firebaseapp.com",
+    projectId: "mega-shop-c110e",
+    storageBucket: "mega-shop-c110e.appspot.com",
+    messagingSenderId: "1039205603450",
+    appId: "1:1039205603450:web:682e0e4b70a32ef76e83c3"
    };
    
    export default firebaseConfig;
