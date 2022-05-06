6cfc82e816530c003f54aee1f5f9fa027f5b036a
okshi <johnstarhafiz@gmail.com>
2022-05-06 18:05:26 +0600

    Redesign some infocard and check responsiveness

--- src/Pages/Home/Home/InfoCard/InfoCard.js
+++ src/Pages/Home/Home/InfoCard/InfoCard.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { CardMedia, Grid } from "@mui/material";
+import { CardMedia, Grid, Box } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
 
 const InfoCard = ({ webInfo }) => {
   const { title, title2, img } = webInfo;
 
+  const useStyles = makeStyles({
+    CardBox: {
+      background: "none",
+      boxShadow: "none",
+      display: "flex",
+      alignItems: "center",
+    },
+  });
+  const classes = useStyles();
+
   return (
     <Grid item xs={12} sm={12} md={3}>
-      <Card
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+      <Card className={classes.CardBox}>
         <CardMedia
-          style={{ width: "16%", height: "16%", marginLeft: "10px" }}
+          style={{ width: "15%", height: "15%" }}
           component="img"
           image={img}
-          alt="green iguana"
+          alt="service talk"
         />
         <CardContent>
           <Typography gutterBottom variant="h6" component="div">
--- src/Pages/Home/Home/InfoCards/InfoCards.js
+++ src/Pages/Home/Home/InfoCards/InfoCards.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import InfoCard from "../InfoCard/InfoCard";
+import { makeStyles } from "@mui/styles";
+import { Container } from "@mui/material";
 
 const InfoCards = () => {
   const [webInfos, setWebInfos] = useState([]);
@@ -12,17 +14,28 @@ const InfoCards = () => {
       .then((data) => setWebInfos(data));
   }, []);
 
+  const useStyles = makeStyles({
+    cardArea: {
+      border: "1px solid #e1e1e1",
+      paddingBottom: "10px",
+      marginTop: "14px",
+    },
+  });
+  const classes = useStyles();
+
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-      sx={{ padding: "16px 0" }}
-    >
-      {webInfos.map((webInfo) => (
-        <InfoCard webInfo={webInfo} key={webInfo.id} />
-      ))}
-    </Grid>
+    <Container>
+      <Grid
+        className={classes.cardArea}
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {webInfos.map((webInfo) => (
+          <InfoCard webInfo={webInfo} key={webInfo.id} />
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
