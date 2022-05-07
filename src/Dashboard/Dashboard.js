ebc0e9c342e4acb8de6f62154399d6bf83f5c501
okshi <johnstarhafiz@gmail.com>
2022-05-08 00:54:42 +0600

    crate react custom dashBoard with material UI

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardTopBar from "./DashboardTopBar/DashboardTopBar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <DashboardTopBar />
+    </div>
+  );
+};
+
+export default Dashboard;
