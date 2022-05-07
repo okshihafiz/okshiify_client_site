6a0214c1ed524726131150a644631df912e2f0fd
okshi <johnstarhafiz@gmail.com>
2022-05-07 16:31:10 +0600

    Finished impliment dynamic rating show in our project

--- src/Pages/Home/BestSell/BestSell.js
+++ src/Pages/Home/BestSell/BestSell.js
@@ -2,6 +2,8 @@ import { CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Card from "@mui/material/Card";
 import React from "react";
+import Rating from "react-rating";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import Rating from "react-rating";
 
 const BestSell = ({ bestData }) => {
@@ -12,6 +14,7 @@ const BestSell = ({ bestData }) => {
       boxShadow: "none",
       display: "flex",
       alignItems: "center",
+      marginBottom: "-14px",
     },
   });
   const classes = useStyles();
@@ -23,7 +26,7 @@ const BestSell = ({ bestData }) => {
     <Grid item xs={12} md={12} sm={12}>
       <Card className={classes.CardBox}>
         <CardMedia
-          style={{ width: "24%", height: "18%" }}
+          style={{ width: "24%", height: "17%" }}
           component="img"
           image={img}
           alt="service talk"
@@ -32,7 +35,13 @@ const BestSell = ({ bestData }) => {
           <Typography gutterBottom variant="body2" color="text.secondary">
             {title}
           </Typography>
-          {/* <Rating initialRating={3} readonly /> */}
+          <Rating
+            style={{ color: "#F9BA48" }}
+            emptySymbol="far fa-star"
+            fullSymbol="fas fa-star"
+            initialRating={ratting}
+            readonly
+          />
           <Typography variant="body1">$ {price}</Typography>
         </CardContent>
       </Card>
